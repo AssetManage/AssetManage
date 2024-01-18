6eaa347eb408632fa73286a524670386e1273258
kimunche <angela_id@naver.com>
2024-01-18 14:26:34 +0900

    first commit

---
+++ src/main/java/com/project/assetManage/AssetManageApplication.java
@@ -0,0 +1,13 @@
+package com.project.assetManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManageApplication.class, args);
+	}
+
+}
