a6868889eba7b5339ad4fe6ee644673026084752
zuxiAqua <shp951226@gmail.com>
2024-02-06 11:22:21 +0900

    [소희][backend] st_user 컬럼 comment 추가

--- src/main/java/com/project/assetManage/entity/User.java
+++ src/main/java/com/project/assetManage/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 @Getter
 @NoArgsConstructor
@@ -14,57 +15,75 @@ public class User extends BaseDateTimeEntity {
     @Id
 //     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="user_seq")
+    @Comment("회원번호")
     private String userSeq;
 
     @Column(name="user_nm")
+    @Comment("성명")
     private String userNm;
 
     @Column(name="login_id")
+    @Comment("로그인ID")
     private String loginId;
 
     @Column(name="login_pw")
+    @Comment("로그인PW")
     private String loginPw;
 
     @Column(name="email")
+    @Comment("이메일")
     private String email;
 
     @Column(name="prdt_rcmd_item_cd")
+    @Comment("상품추천항목코드")
     private String prdtRcmdItemCd;
 
     @Column(name="lock_yn")
+    @Comment("잠금여부")
     private Character lockYn;
 
     @Column(name="secsn_yn")
+    @Comment("탈퇴여부")
     private Character secsnYn;
 
     @Column(name="indvdlinfo_agree_yn")
+    @Comment("개인정보동의여부")
     private Character indvdlinfoAgreeYn;
 
     @Column(name="profile_img_url")
+    @Comment("프로필이미지url")
     private String profileImgUrl;
 
     @Column(name="sex_cd")
+    @Comment("성별")
     private String sexCd;
 
     @Column(name="age_cd")
+    @Comment("연령대")
     private String ageCd;
 
     @Column(name="age")
+    @Comment("나이")
     private Integer age;
 
     @Column(name="occupation_cd")
+    @Comment("직업군")
     private String occupationCd;
 
     @Column(name="mobile_tel_num")
+    @Comment("핸드폰번호")
     private String mobileTelNum;
 
     @Column(name="zip_cd")
+    @Comment("우편번호")
     private String zipCd;
 
     @Column(name="zip_detail_addr1")
+    @Comment("상세주소1")
     private String zipDetailAddr1;
 
     @Column(name = "zip_detail_addr2")
+    @Comment("상세주소2")
     private String zipDetailAddr2;
 
 
