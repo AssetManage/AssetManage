fc9c2a20767bfce1dfa72ebeab3a1496f17f089f
kimunche <angela_id@naver.com>
2024-03-13 12:34:43 +0900

    fix : SignController api operation 작성

--- src/main/java/com/project/assetManage/controller/SignController.java
+++ src/main/java/com/project/assetManage/controller/SignController.java
@@ -4,6 +4,7 @@ import com.project.assetManage.dto.SignRequest;
 import com.project.assetManage.dto.SignResponse;
 import com.project.assetManage.dto.SignoutRequest;
 import com.project.assetManage.service.SignService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +18,13 @@ public class SignController {
         this.signService = signService;
     }
 
+    @Operation(summary = "회원가입", description = "신규 유저 회원가입")
     @PostMapping("/join")
     public SignResponse join(@RequestBody SignRequest request) {
         return signService.join(request);
     }
 
+    @Operation(summary = "로그인", description = "기존 유저 로그인")
     @PostMapping("/login")
     public SignResponse login(@RequestBody SignRequest request){
         return signService.login(request);
@@ -33,6 +36,7 @@ public class SignController {
     }
 
 
+    @Operation(summary = "이메일 중복체크", description = "이메일을 중복체크한다.")
     @GetMapping("/emailCheck")
     public ResponseEntity<Boolean> emailDuplicateCheck(@RequestParam("email") String email){
         return ResponseEntity.ok(signService.emailCheck(email));
