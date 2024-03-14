0174ddfa803a66bac0ef7b3e2e7e414f07b88d4c
Park Sungju <dkss7413@naver.com>
2024-03-14 14:04:36 +0900

    [성주][frontend] 헤더(공통 컴포넌트) 개발

--- src/main/frontend/src/components/common/header/Header.js
+++ src/main/frontend/src/components/common/header/Header.js
@@ -2,47 +2,100 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import styles from "./Header.module.css";
+import styles from './Header.module.css';
 
-import { checkLoginState } from "../../../common/common";
+import { checkLoginState } from '../../../common/common';
 
 function Header() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const goToPage = (page) => {
-        navigate(page);
+    const [loginState, setLoginState] = useState(false);
+    const [userName, setUserName] = useState('');
+    const [profileImgUrl, setProfileImgUrl] = useState('');
+
+    // 로그인
+    const login = () => {
+        sessionStorage.setItem('preLoginPath', window.location.pathname);
+        navigate('/login');
     };
 
-    useEffect(() => {
-    }, []);
+    // 로그아웃
+    const logout = async () => {
+        localStorage.removeItem('accessToken');
+        window.location.reload();
+    };
+
+	useEffect(() => {
+		const onStart = async () => {
+			const response = await axios
+				.get('/st/user/selectUserAll', {
+					headers: {
+						Authorization: localStorage.getItem('accessToken'),
+					},
+				})
+				.then(function (res) {
+				    console.log(res);
+					return res;
+				})
+				.catch(function (error) {
+					return error;
+				});
+
+            // 비로그인 상태
+			if(response?.data?.stat !== 'SUCCESS'){
+			    setLoginState(false);
+			}
+			// 로그인 상태
+			else{
+                setLoginState(true);
+                setUserName(response?.data?.data?.userNm);
+
+                if(response?.data?.data?.profileImgUrl === '' || response?.data?.data?.profileImgUrl === null){
+                    setProfileImgUrl('/userIcon.svg');
+                } else {
+                    setProfileImgUrl(response?.data?.data?.profileImgUrl);
+                }
+			}
+		};
+
+		onStart();
+	}, []);
 
 	return (
-	    <div>
-            <div className={styles.logo}>LOGO</div>
-            <div className={styles.menu}>
-                <div className={styles.menu01}>menu01</div>
-                <div className={styles.menu02}>menu02</div>
-                <div className={styles.menu03}>menu03</div>
-            </div>
-            {/*
-            <div className={styles.div}>
-                <img className={styles.vectorIcon} alt="" src="/vector.svg" />
-                <b className={styles.logOut}>Log out</b>
-                <img className={styles.groupIcon} alt="" src="/group.svg" />
-                <div className={styles.kimsw1245}>김선우(kimsw1245)</div>
-            </div>
-            */}
+		<div>
+			<div className={styles.logo} onClick={() => navigate('/main')}>LOGO</div>
+			<div className={styles.menu}>
+				<div className={styles.menu01} onClick={() => navigate('/main')}>예금</div>
+				<div className={styles.menu02} onClick={() => navigate('/main')}>적금</div>
+				<div className={styles.menu03} onClick={() => navigate('/main')}>추천</div>
+			</div>
+			{/* 비로그인 상태 */}
+			{!loginState &&
 			<div className={styles.vectorParent}>
-			    <div className={styles.vectorDiv} onClick={() => goToPage('/login')}>
-                    <img className={styles.vectorIcon2} alt="" src="/vector.svg" />
-                    <b className={styles.logIn}>Log in</b>
+				<div className={styles.vectorDiv} onClick={() => login()}>
+					<img className={styles.vectorIcon2} alt='' src='/vector.svg' />
+					<b className={styles.logIn}>Log in</b>
 				</div>
-				<div className={styles.vectorDiv} onClick={() => goToPage('/sign-up')}>
-                    <img className={styles.vectorIcon1} alt="" src="/userIcon.svg" />
-                    <b className={styles.signUp}>Sign up</b>
+				<div className={styles.vectorDiv} onClick={() => navigate('/sign-up')}>
+					<img className={styles.vectorIcon1} alt='' src='/userIcon.svg' />
+					<b className={styles.signUp}>Sign up</b>
 				</div>
 			</div>
-        </div>
+			}
+			{/* 로그인 상태 */}
+			{loginState &&
+            <div className={styles.div}>
+				<div className={styles.vectorDiv} onClick={() => logout()}>
+                    <img className={styles.vectorIcon} alt='' src='/vector.svg' />
+                    <b className={styles.logOut}>Log out</b>
+                </div>
+				<div className={styles.vectorDiv} onClick={() => {}}>
+                    <img className={styles.groupIcon} alt='' src={profileImgUrl} />
+                    <div className={styles.kimsw1245}>{userName}</div>
+                </div>
+            </div>
+            }
+		</div>
 	);
 }
 
