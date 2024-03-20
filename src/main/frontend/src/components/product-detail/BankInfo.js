1efbf224cfccd19bfa05087ffb473b3bdd5c4343
jiwonson1 <jiwonson.dev@gmail.com>
2024-03-20 17:41:40 +0900

    [지원][frontend] detail 탭 수정 및 이미지 추가

--- src/main/frontend/src/components/product-detail/BankInfo.js
+++ src/main/frontend/src/components/product-detail/BankInfo.js
@@ -44,24 +44,68 @@ const BankInfo = () => {
 
     return (
         <div className="div28">
-            <div className="div29">상품안내</div>
-            <div className="bg-box6"/>
-            <div className="product-detail">
-                <div className="product-info">
-                    <table {...getTableProps()} className="react-table">
-                        <tbody {...getTableBodyProps()}>
-                        {rows.map(row => {
-                            prepareRow(row);
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map(cell => (
-                                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                    ))}
-                                </tr>
-                            );
-                        })}
-                        </tbody>
-                    </table>
+            <div className="tab_title">
+                <div className="div29">상품안내</div>
+                <div className="bg-box6"/>
+            </div>
+            <div className="product-info" id="product-info">
+                <table {...getTableProps()} className="react-table">
+                    <tbody {...getTableBodyProps()}>
+                    {rows.map(row => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => (
+                                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                ))}
+                            </tr>
+                        );
+                    })}
+                    </tbody>
+                </table>
+            </div>
+            <div className="tab_title">
+                <div className="div29">금리안내</div>
+                <div className="bg-box6"/>
+            </div>
+            <div className="product-info" id="react-table2">
+                <table {...getTableProps()} className="react-table2" >
+                    <thead>
+                    <tr>
+                        <th>기간</th>
+                        <th>금리(연, 세전)</th>
+                        <th>복리수익률(연, 세전)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {productData && productData.list[0]?.productOptionList.slice().reverse().map(option => (
+                        <tr key={option.optionId}>
+                            <td>{option.saveTrm}개월</td>
+                            <td>{option.intrRate}%</td>
+                            <td>{option.intrRate2}%</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
+            <div className="tab_title">
+                <div className="div29">유의사항</div>
+                <div className="bg-box6"/>
+            </div>
+            <div className="product-info" id="notice-detail">
+                <b className= "notice-title">※ 계약 체결 전 상품설명서 및 약관을 확인하시기 바랍니다.</b>
+                <div className="notice-detail"></div>
+            </div>
+            <div className="product-info-footer">
+                <div className="logo-img">
+                    <img className="logo" alt="" src="/depositor-protection.png"/>
+                </div>
+                <div className="depositor-protection">
+                    <b className="semi-title">예금자 보호 안내</b>
+                    <br/>
+                    <span className="protect-detail">이 예금은 예금자보호법에 따라 예금보험공사가 보호하되, 보호 한도는 본 저축은행에 있는 귀하의 모든 예금보호 대상 금융상품의
+원금과 소정의 이자를 합하여 1인당 “최고 5천만원”이며, 5천만원을초과하는 나머지 금액은 보호하지 않습니다.</span>
                 </div>
             </div>
         </div>
--- src/main/frontend/src/components/product-detail/Tab.js
+++ src/main/frontend/src/components/product-detail/Tab.js
@@ -19,26 +19,38 @@ const Tab = () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []); // 컴포넌트가 처음 렌더링될 때만 실행
+
+    const scrollToSection = (id) => {
+        const element = document.getElementById(id);
+        if (element) {
+            window.scrollTo({
+                top: element.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    };
+
+
     return (
         <div className={`tab-menu ${scrolled ? 'scrolled' : ''}`}>
             <div className="in-box-parent">
-                <div className="in-box2">
-                    <div className="div25"><b className="b13">상품안내</b></div>
-                    <img className="arr-ico-icon" alt="" src="/arrico_white.svg"/>
+                <div className="in-box" onClick={() => scrollToSection('product-info')}>
+                    <div className="div25">상품안내</div>
+                    <img className="arr-ico-icon" alt="" src="/arrico.svg"/>
                 </div>
 
             </div>
-            <div className="in-box-group">
-                <div className="in-box3">
-                    <div className="div26">금리안내</div>
-                    <img className="arr-ico-icon1" alt="" src="/arrico.svg"/>
+            <div className="in-box-parent">
+                <div className="in-box" onClick={() => scrollToSection('react-table2')}>
+                    <div className="div25">금리안내</div>
+                    <img className="arr-ico-icon" alt="" src="/arrico.svg"/>
                 </div>
 
             </div>
-            <div className="in-box-container">
-                <div className="in-box4">
-                    <div className="div27">유의사항</div>
-                    <img className="arr-ico-icon2" alt="" src="/arrico.svg"/>
+            <div className="in-box-parent">
+                <div className="in-box" onClick={() => scrollToSection('notice-detail')}>
+                    <div className="div25">유의사항</div>
+                    <img className="arr-ico-icon" alt="" src="/arrico.svg"/>
                 </div>
 
             </div>
